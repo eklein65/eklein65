6a3bea38e22de3ef3f66e1748ba15b5fc0c0d5da
Eric <ek1051@unh.edu>
2022-01-10 12:40:24 -0500

    Fixed syntax errors

--- Blink3.py
+++ Blink3.py
@@ -12,7 +12,7 @@ import random
 
 ledPin1 = 11
 ledPin2 = 13
-ledPin3 = 15 # define ledPins
+ledPin3 = 15  # define ledPins
 ledPins = array([ledPin1, ledPin2, ledPin3])
 buttonPin1 = 35
 buttonPin2 = 37
@@ -21,26 +21,30 @@ buttonPins = array([buttonPin1, buttonPin2, buttonPin3])
 
 score = 0
 
+
 def setup():
     for ledPin in ledPins:
         GPIO.setmode(GPIO.BOARD)  # use PHYSICAL GPIO Numbering
         GPIO.setup(ledPin, GPIO.OUT)  # set the ledPin to OUTPUT mode
         GPIO.output(ledPin, GPIO.LOW)  # make ledPin output LOW level
         print('using pin%d as led' % ledPin)
-    GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    print ('using pin%d as button'%ledPin)
+    for buttonPin in buttonPins:
+        GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        print('using pin%d as button' % ledPin)
+
 
 def loop():
     while True:
         for ledPin in ledPins:
             GPIO.output(ledPin, GPIO.HIGH)  # make ledPin output HIGH level to turn on led
-            print ('led turned on >>>')     # print information on terminal
-    
-        time.sleep(1)                       # Wait for 1 second
+            print('led turned on >>>')  # print information on terminal
+
+        time.sleep(1)  # Wait for 1 second
         for ledPin in ledPins:
-            GPIO.output(ledPin, GPIO.LOW)   # make ledPin output LOW level to turn off led
-            print ('led turned off <<<')
-        time.sleep(1)                   # Wait for 1 second
+            GPIO.output(ledPin, GPIO.LOW)  # make ledPin output LOW level to turn off led
+            print('led turned off <<<')
+        time.sleep(1)  # Wait for 1 second
+
 
 def whackamole():
     min = 0
@@ -48,6 +52,7 @@ def whackamole():
 
     while true:
         randLedOn(random.uniform(min, max))
+
 
 def randLedOn(sec):
     global score
@@ -62,26 +67,25 @@ def randLedOn(sec):
     timeout_start = time.time()
 
     while time.time() < timeout_start + timeout:
-        if GPIO.input(buttonPin) == GPIO.LOW: #If button pressed in time,
+        if GPIO.input(buttonPin) == GPIO.LOW:  # If button pressed in time,
             GPIO.output(ledPin, GPIO.LOW)
             score += 1
             break
-
-
 
     # if user input on correct light
     #   turn off light and return with value of 1
     # else
     #   keep on full time and return with value of 0
 
-def destroy():
-    GPIO.cleanup()                      # Release all GPIO
 
-if __name__ == '__main__':    # Program entrance
-    print ('Program is starting ... \n')
+def destroy():
+    GPIO.cleanup()  # Release all GPIO
+
+
+if __name__ == '__main__':  # Program entrance
+    print('Program is starting ... \n')
     setup()
     try:
         whackamole()
-    except KeyboardInterrupt:   # Press ctrl-c to end the program.
+    except KeyboardInterrupt:  # Press ctrl-c to end the program.
         destroy()
-
